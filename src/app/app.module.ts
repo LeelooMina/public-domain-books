3e14e7fa2666790e81590d3a07b99479a0654b43
Crystal Campbell <blankpage@gmail.com>
2022-09-25 11:25:34 -0500

    ADD: Components

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,22 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HeaderComponent } from './header/header.component';
+import { NavbarComponent } from './navbar/navbar.component';
+import { BooksComponent } from './books/books.component';
+import { DisplaybookComponent } from './displaybook/displaybook.component';
+import { DisplayBookComponent } from './books/display-book/display-book.component';
+import { SearchBarComponent } from './search-bar/search-bar.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    NavbarComponent,
+    BooksComponent,
+    DisplaybookComponent,
+    DisplayBookComponent,
+    SearchBarComponent
   ],
   imports: [
     BrowserModule,
