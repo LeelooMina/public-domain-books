860dde5772f99bc255378ed0d72683d86c41df6f
Crystal Campbell <blankpage@gmail.com>
2022-11-22 22:28:37 -0600

    Chore: Setting up/adding components

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,22 +3,28 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { HeaderComponent } from './header/header.component';
 import { NavbarComponent } from './navbar/navbar.component';
 import { BooksComponent } from './books/books.component';
-import { DisplaybookComponent } from './displaybook/displaybook.component';
 import { DisplayBookComponent } from './books/display-book/display-book.component';
 import { SearchBarComponent } from './search-bar/search-bar.component';
+import { LoginComponent } from './login/login.component';
+import { UserProfileComponent } from './user-profile/user-profile.component';
+import { SavedBooksComponent } from './saved-books/saved-books.component';
+import { FooterComponent } from './footer/footer.component';
+import { HomepageComponent } from './homepage/homepage.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    HeaderComponent,
     NavbarComponent,
     BooksComponent,
-    DisplaybookComponent,
     DisplayBookComponent,
-    SearchBarComponent
+    SearchBarComponent,
+    LoginComponent,
+    UserProfileComponent,
+    SavedBooksComponent,
+    FooterComponent,
+    HomepageComponent
   ],
   imports: [
     BrowserModule,
